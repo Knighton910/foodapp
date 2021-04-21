2cd6932531d8c68030b559e89228fe25a5b7b5ca
Knighton910 <keltheceo@gmail.com>
2021-04-21 12:39:14 -0700

    added details page initial code

---
+++ components/Details.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Text, View } from 'react-native'
+
+export default Details = () => {
+    return (
+        <View>
+            <Text>Details Page</Text>
+        </View>
+    )
+}
--- readme.md
+++ readme.md
@@ -3,3 +3,9 @@ Start Metro
 ```
 npx react-native start
 ```
+
+in a new terminal
+
+```
+npm run ios
+```
